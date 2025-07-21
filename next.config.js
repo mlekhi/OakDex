a324d4c388ca423241b7f90c7291d505a7b46538
mlekhi <maya.lekhi1@gmail.com>
2025-07-20 21:38:47 -0700

    Secure headers

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { // no embedding/framing
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          { // no loading resources from other domains
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          { // url privacy protection
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          { // load resources from trusted sources
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://api.tcgdex.net https://api.openai.com https://api.pinecone.io;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig; 
